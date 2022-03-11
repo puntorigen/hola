60effdb22d48c305f53033b6093a0d6fc1e4a7f0
Pablo Schaffner <pablo@puntorigen.com>
2022-03-11 00:37:26 -0300

    improved templates cmd

--- src/cmds/templates.ts
+++ src/cmds/templates.ts
@@ -24,12 +24,15 @@ export default class Templates extends Command {
             const editor = require('tiny-cli-editor');
             let message = editor(text);
             message.on('data',(text_) => {
+                console.log('data',text_);
                 resolve(text_);
             })
             message.on('abort',(text_) => {
+                console.log('abort',text_);
                 reject(text_);
             })
             message.on('submit',(text_) => {
+                console.log('submit',text_);
                 resolve(text_);
             })
         });
@@ -46,13 +49,36 @@ export default class Templates extends Command {
         } else if (this.action=='create') {
             if (!questions.name) questions.name = await this.ask(`How do you plan to name this new template?`);
             questions.keywords = await this.ask(`Enter the keywords for searching new members:`);
+            questions.keywords_exclude = await this.ask(`Enter the keywords that profiles shouldn't have:`);
             const countries = $enum(CountryType).map((value)=>({ title:CountryType[value], value }) );
-            questions.country = await this.multipleChoice(`What countries should the people be from?`, countries, 3);
+            questions.countries = await this.multipleChoice(`What countries should the people be from?`, countries, 3);
             questions.exclude = await this.ask(`List people to exclude (comma delimited):`);
             if (questions.exclude!='') questions.exclude=questions.exclude.split(','); 
-            questions.numberLines = await this.ask(`How many message templates do you wish to write now?`,(value)=>parseInt(value)==value);
-            if (questions.numberLines!='') questions.numberMessages = parseInt(questions.numberLines);
-            questions.message = await this.editor(`Hola {firstName},\nMi nombre es {myFirstname} y te invite porque me parecio interesante tu perfil para compartirlo con la empresa que trabajo.\nSolo piden que sepas ingles y tengas experiencia en NodeJS.`);
+            questions.messageNow = await this.choose(`Do you want to add a message template now?`,[{ title:'Yes',value:true },{ title:'No',value:false }]);
+            this.debug('You can use variables: {firstName}, {lastName}, {myFirstName}');
+            questions.message = '';
+            if (questions.messageNow) {
+                questions.message = [];
+                for (const x of new Array(100)) {
+                    questions.message.push(await this.ask(`Line ${questions.message.length+1}:`));
+                    if (questions.message.length>2) {
+                        //if last 2 lines are empty, stop querying lines
+                        if (questions.message[questions.message.length-1]=='' && questions.message[questions.message.length-2]=='') break;
+                    }
+                }
+                questions.message = questions.message.filter(String).join('\n'); //(item)=>(item.trim()!='')
+            }
+            //add to DB
+            let add = await db.push('templates',{ name:questions.name, 
+                                        exclude:questions.keywords_exclude, //exclude keywords from profiles 
+                                        country:questions.countries,
+                                        exclude_people:questions.exclude,   //don't include this people
+                                        max_grow:10,
+                                        max_invite:30,
+                                        invitation_message: questions.message
+                                    });
+            this.debug('added ',add);
+            await db.save();
             //
             this.debug('debug answers',questions);
         }
